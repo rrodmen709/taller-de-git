c7ae01957acf2d973db1ec29beed61e9eebd9ecf
rafarm <rrodmen709@g.educaand.es>
2023-10-17 10:03:32 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,6 @@
 <?php
 //Autor: Rafa <rrodmen709@g.educaand.es>
-//El nombre por defecto es mundo
-$nombre = isset($argv[1]) ? $argv[1]: "Mundo";
+print "Introduce tu nombre: ";
+$nombre = trim(fgets(STDIN));
 @print "Hola {$nombre}\n";
 ?>
